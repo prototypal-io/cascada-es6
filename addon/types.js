c9395a41ee88e9d209eccc70e1f370d049fb3157
James Fickel <jamesfickel@gmail.com>
2015-08-11 14:44:23 -0700

    Move Cascada to ES6

---
+++ addon/types.js
@@ -0,0 +1,49 @@
+import css from '../parse-css';
+
+function tok(source) {
+  return {
+    type: 'Token',
+    source: source
+  }
+}
+
+function number(value) {
+  return {
+    type: 'Number',
+    value: value,
+    source: value,
+  }
+}
+
+function ident(name) {
+  return {
+    type: 'Ident',
+    name: name,
+    source: name
+  };
+}
+
+function func(name, args) {
+  return {
+    type: 'Function',
+    name: name,
+    args: args
+  };
+}
+
+function ws() {
+  return tok(' ');
+}
+
+function comma() {
+  return tok(',');
+}
+
+export {
+  tok,
+  ws,
+  comma,
+  number,
+  ident,
+  func
+}
