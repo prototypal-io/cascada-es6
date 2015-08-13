3f30f69e4597d27446f59b9bebed31f65eed5c0a
James Fickel <jamesfickel@gmail.com>
2015-08-12 19:53:41 -0700

    created parser class, storing custom functionson parser instances and handling whitespace/comma tokens for custom functions

--- addon/index.js
+++ addon/index.js
@@ -1,210 +1,251 @@
 import css from './parse-css';
 
+class Parser {
+  constructor() {
+    this.customFunctions = {};
+  }
+
+  acceptStylesheet(node) {
+    return {
+      type: 'StyleSheet',
+      rules: this.acceptRules(node.value)
+    };
+  }
+
+  acceptRules(nodes) {
+    return nodes.map(this.acceptRule.bind(this));
+  }
+
+  acceptRule(node) {
+    switch (node.type) {
+      case 'QUALIFIED-RULE': return this.acceptStyleRule(node);
+      case 'AT-RULE':        return this.acceptAtRule(node);
+      default: throw new Error("Unknown rule type: " + node.type);
+    }
+  }
+
+  acceptStyleRule(node) {
+    return {
+      type: 'StyleRule',
+      selectors: [ this.acceptSelector(node.prelude) ],
+      body: this.acceptDeclarations(node.value.value, this.acceptStyleDeclaration.bind(this))
+    }
+  }
+
+  acceptSelector(tokens) {
+    return tokens.map(function (token) {
+      return token.toSource();
+    }).join('').trimRight();
+  }
+
+  acceptDeclarations(tokens, acceptor) {
+    var nodes = css.consumeAListOfDeclarations(
+      new css.TokenStream(tokens)
+    );
+
+    return nodes.map(function (node) {
+      return acceptor(node);
+    });
+  }
+
+  acceptStyleDeclaration(node) {
+    switch (node.type) {
+      case 'DECLARATION': return this.acceptStylePropertyDeclaration(node);
+      case 'AT-RULE':     return this.acceptAtRule(node);
+      default: throw new Error("Unknown style declaration type: " + node.type);
+    }
+  }
+
+  acceptStylePropertyDeclaration(node) {
+    return {
+      type: 'StylePropertyDeclaration',
+      important: node.important,
+      name: node.name,
+      values: this.acceptComponentValues(node.value)
+    };
+  }
+
+  acceptComponentValues(parts) {
+    return this.trimWhitespaceTokens(parts).map(this.acceptComponentValue.bind(this));
+  }
+
+  acceptComponentValue(node) {
+    switch (node.tokenType) {
+      case 'WHITESPACE': return this.acceptToken(node);
+      case 'DELIM':      return this.acceptToken(node);
+      case ',':          return this.acceptToken(node);
+      case 'IDENT':      return this.acceptIdent(node);
+      case 'HASH':       return this.acceptHash(node);
+      case 'STRING':     return this.acceptString(node);
+      case 'NUMBER':     return this.acceptNumber(node);
+      case 'DIMENSION':  return this.acceptDimension(node);
+      case 'PERCENTAGE': return this.acceptPercentage(node);
+    }
+
+    switch (node.type) {
+      // case 'integer':    return acceptInteger(node);
+      case 'FUNCTION':   return this.acceptFunction(node);
+    }
+
+    throw new Error("Unknown component value: <" + node.tokenType + ", " + node.type + ">");
+  }
+
+  acceptToken(node) {
+    return {
+      type: "Token",
+      source: node.toSource()
+    };
+  }
+
+  acceptIdent(node) {
+    return {
+      type: "Ident",
+      name: node.value,
+      source: node.toSource()
+    };
+  }
+
+  acceptHash(node) {
+    return {
+      type: "Hash",
+      value: node.value,
+      source: node.toSource()
+    };
+  }
+
+  acceptString(node) {
+    return {
+      type: "String",
+      value: node.value,
+      source: node.toSource()
+    };
+  }
+
+  acceptNumber(node) {
+    return {
+      type: "Number",
+      value: node.value,
+      source: node.toSource()
+    };
+  }
+
+  acceptDimension(node) {
+    return {
+      type: "Dimension",
+      value: node.value,
+      unit: node.unit,
+      source: node.toSource()
+    };
+  }
+
+  acceptPercentage(node) {
+    return {
+      type: "Percentage",
+      value: node.value,
+      source: node.toSource()
+    };
+  }
+
+  registerFunction(name, callback) {
+    this.customFunctions[name] = callback;
+  }
+
+  acceptFunction(node) {
+    if (this.customFunctions[node.name]) {
+      return {
+        type: "Function",
+        name: node.name,
+        args: this.acceptCommaSeparatedComponentValues(node.value)
+      };
+    } else {
+      return {
+        type: "Function",
+        name: node.name,
+        args: this.acceptComponentValues(node.value)
+      };
+    }
+  }
+
+  acceptCommaSeparatedComponentValues(parts) {
+    let whitespacelessParts = this.removeWhitespaceTokens(parts);
+    let cleanParts = this.removeCommaTokens(whitespacelessParts);
+    return cleanParts.map(this.acceptComponentValue.bind(this));
+  }
+
+  removeWhitespaceTokens(tokens) {
+    return tokens.filter(function (token) {
+      return token.tokenType !== 'WHITESPACE';
+    });
+  }
+
+  removeCommaTokens(tokens) {
+    return tokens.filter(function (token) {
+      return token.tokenType !== ',';
+    });
+  }
+
+  acceptComponentValueParts(name, parts) {
+    return parts.map(this.acceptComponentValuePart);
+  }
+
+  acceptComponentValuePart(node) {
+    if (node.tokenType) {
+      return node;
+    }
+
+    switch (node.type) {
+      case 'FUNCTION': return this.acceptFunction(node);
+      default: throw new Error("Unknown style part: " + node.tokenType + " " + node.type + " " + node)
+    }
+  }
+
+  printValue(parts) {
+    return parts.map(function (part) {
+      if (part.tokenType) {
+        return part.toSource();
+      } else if (part.type === 'FUNCTION') {
+        return part.name + '(' + this.printValue(part.value) + ')';
+      } else {
+        throw new Error("Unsupported value part: " + JSON.stringify(part.toJSON()));
+      }
+    }).join('');
+  }
+
+  acceptMixinStyleDeclaration(node) {
+    switch (node.type) {
+      case 'DECLARATION': return this.acceptStylePropertyDeclaration(node);
+      default: throw new Error("Unknown style declaration type: " + node.type);
+    }
+  }
+
+  acceptAtRule(node) {
+    return {
+      type: 'AtRule',
+      name: node.name,
+      prelude: node.prelude,
+      value: node.value
+    };
+  }
+
+  trimWhitespaceTokens(tokens) {
+    var startIndex = 0;
+    var endIndex = tokens.length;
+
+    while (tokens[startIndex].tokenType === 'WHITESPACE') { startIndex++; }
+    while (tokens[endIndex-1].tokenType === 'WHITESPACE') { endIndex--; }
+
+    return tokens.slice(startIndex, endIndex);
+  }
+}
+
+let parser = new Parser();
+
 export function parse(source) {
-  return acceptStylesheet(
+  return parser.acceptStylesheet(
     css.parseAStylesheet(source)
   );
 };
 
-function acceptStylesheet(node) {
-  return {
-    type: 'StyleSheet',
-    rules: acceptRules(node.value)
-  };
-}
-
-function acceptRules(nodes) {
-  return nodes.map(acceptRule);
-}
-
-function acceptRule(node) {
-  switch (node.type) {
-    case 'QUALIFIED-RULE': return acceptStyleRule(node);
-    case 'AT-RULE':        return acceptAtRule(node);
-    default: throw new Error("Unknown rule type: " + node.type);
-  }
-}
-
-function acceptStyleRule(node) {
-  return {
-    type: 'StyleRule',
-    selectors: [ acceptSelector(node.prelude) ],
-    body: acceptDeclarations(node.value.value, acceptStyleDeclaration)
-  }
-}
-
-function acceptSelector(tokens) {
-  return tokens.map(function(token) {
-    return token.toSource();
-  }).join('').trimRight();
-}
-
-function acceptDeclarations(tokens, acceptor) {
-  var nodes = css.consumeAListOfDeclarations(
-    new css.TokenStream(tokens)
-  );
-
-  return nodes.map(function(node) {
-    return acceptor(node);
-  });
-}
-
-function acceptStyleDeclaration(node) {
-  switch (node.type) {
-    case 'DECLARATION': return acceptStylePropertyDeclaration(node);
-    case 'AT-RULE':     return acceptAtRule(node);
-    default: throw new Error("Unknown style declaration type: " + node.type);
-  }
-}
-
-function acceptStylePropertyDeclaration(node) {
-  return {
-    type: 'StylePropertyDeclaration',
-    important: node.important,
-    name: node.name,
-    values: acceptComponentValues(node.value)
-  };
-}
-
-function acceptComponentValues(parts) {
-  return trimWhitespaceTokens(parts).map(acceptComponentValue);
-}
-
-function acceptComponentValue(node) {
-  switch (node.tokenType) {
-    case 'WHITESPACE': return acceptToken(node);
-    case 'DELIM':      return acceptToken(node);
-    case ',':          return acceptToken(node);
-    case 'IDENT':      return acceptIdent(node);
-    case 'HASH':       return acceptHash(node);
-    case 'STRING':     return acceptString(node);
-    case 'NUMBER':     return acceptNumber(node);
-    case 'DIMENSION':  return acceptDimension(node);
-    case 'PERCENTAGE': return acceptPercentage(node);
-  }
-
-  switch (node.type) {
-    // case 'integer':    return acceptInteger(node);
-    case 'FUNCTION':   return acceptFunction(node);
-  }
-
-  throw new Error("Unknown component value: <" + node.tokenType + ", " + node.type + ">");
-}
-
-function acceptToken(node) {
-  return {
-    type: "Token",
-    source: node.toSource()
-  };
-}
-
-function acceptIdent(node) {
-  return {
-    type: "Ident",
-    name: node.value,
-    source: node.toSource()
-  };
-}
-
-function acceptHash(node) {
-  return {
-    type: "Hash",
-    value: node.value,
-    source: node.toSource()
-  };
-}
-
-function acceptString(node) {
-  return {
-    type: "String",
-    value: node.value,
-    source: node.toSource()
-  };
-}
-
-function acceptNumber(node) {
-  return {
-    type: "Number",
-    value: node.value,
-    source: node.toSource()
-  };
-}
-
-function acceptDimension(node) {
-  return {
-    type: "Dimension",
-    value: node.value,
-    unit: node.unit,
-    source: node.toSource()
-  };
-}
-
-function acceptPercentage(node) {
-  return {
-    type: "Percentage",
-    value: node.value,
-    source: node.toSource()
-  };
-}
-
-function acceptFunction(node) {
-  return {
-    type: "Function",
-    name: node.name,
-    args: acceptComponentValues(node.value)
-  };
-}
-
-function printValue(parts) {
-  return parts.map(function(part) {
-    if (part.tokenType) {
-      return part.toSource();
-    } else if (part.type === 'FUNCTION') {
-      return part.name + '(' + printValue(part.value) + ')';
-    } else {
-      throw new Error("Unsupported value part: " + JSON.stringify(part.toJSON()));
-    }
-  }).join('');
-}
-
-function acceptComponentValueParts(name, parts) {
-  return parts.map(acceptComponentValuePart);
-}
-
-function acceptComponentValuePart(node) {
-  if (node.tokenType) {
-    return node;
-  }
-
-  switch (node.type) {
-    case 'FUNCTION': return acceptFunction(node);
-    default: throw new Error("Unknown style part: " + node.tokenType + " " + node.type + " " + node)
-  }
-}
-
-function acceptMixinStyleDeclaration(node) {
-  switch (node.type) {
-    case 'DECLARATION': return acceptStylePropertyDeclaration(node);
-    default: throw new Error("Unknown style declaration type: " + node.type);
-  }
-}
-
-
-function acceptAtRule(node) {
-  return {
-    type: 'AtRule',
-    name: node.name,
-    prelude: node.prelude,
-    value: node.value
-  };
-}
-
-function trimWhitespaceTokens(tokens) {
-  var startIndex = 0;
-  var endIndex = tokens.length;
-
-  while (tokens[startIndex].tokenType === 'WHITESPACE') { startIndex++; }
-  while (tokens[endIndex-1].tokenType === 'WHITESPACE') { endIndex--; }
-
-  return tokens.slice(startIndex, endIndex);
+export function registerFunction(name, callback) {
+  parser.registerFunction(name, callback)
 }
